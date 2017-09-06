0b582b623af70f1a1e73762755d4e0c5bab92a26
maranemil <maran_emil@yahoo.com>
2017-09-06 21:13:57 +0200

    code source

---
+++ string2json.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lime
+ * Date: 06.09.17
+ * Time: 21:12
+ */
+
+
+// http://phptester.net/
+$str = "Club-Haus, Alter Bahnhof, Alter Botanischer Garten, Alter Markt, Altstadt";
+$strtmp = explode(",", $str);
+echo "<textarea>";
+echo json_encode($strtmp,true);
